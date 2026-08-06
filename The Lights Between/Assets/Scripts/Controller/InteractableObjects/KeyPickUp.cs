0229482aa80194ccee05378f95bf0ae577919d72
M7UNDO <165899552+M7UNDO@users.noreply.github.com>
2026-08-06 19:05:22 +0200

    Level Layout

---
+++ The Lights Between/Assets/Scripts/Controller/InteractableObjects/KeyPickUp.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class KeyPickUp : MonoBehaviour, IInteractable
+{
+    public string KeyID = "GarageKey";
+
+    [Header("SFX")]
+
+    public AudioClip pickUpSFX;
+    public void Interact()
+    {
+        PickupKey();
+    }
+
+    public void PickupKey()
+    {
+        GameObject player = GameObject.Find("Player");
+
+        if (player != null && player.TryGetComponent<PlayerKeyInventory>(out PlayerKeyInventory keyInventory))
+        {
+            keyInventory.AddKey(KeyID);
+
+            if(pickUpSFX != null)
+            {
+                AudioSource.PlayClipAtPoint(pickUpSFX, transform.position);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
